54712ae91284f385b1a63cab06993e3e5d38d43a
Andrey Kolyada <andrey.kolyada.00@gmail.com>
2025-07-15 13:01:03 +0500

    Using lm studio server for ai answers

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <httplib.h>
+#include <nlohmann/json.hpp>
 #include <string>
 #include <fstream>
+#include <codecvt>
+#include <io.h>
+#include <fcntl.h>
+
+using json = nlohmann::json;
 
 std::string readFileToString(const std::string& filepath) {
 	std::ifstream file(filepath);
@@ -22,20 +28,79 @@ std::string readFileToString(const std::string& filepath) {
 	return content;
 }
 
+std::string getDateTime() {
+	std::time_t now = std::time(nullptr);
+	std::tm* localTime = std::localtime(&now);
+	
+	std::ostringstream oss;
+	oss << std::put_time(localTime, "%Y-%m-%d %H:%M");
+	
+	return oss.str();
+}
+
+std::wstring toWstring(std::string& str) {
+	std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+	return converter.from_bytes(str);
+}
+
 int main() {
+	std::cout << "Server starting... ";
+
+	//_setmode(_fileno(stdout), _O_U16TEXT);
+	//_setmode(_fileno(stdin), _O_U16TEXT);
+
 	httplib::Server svr;
-	std::string html;
+	httplib::Client cli("http://localhost:1234");
 
-	try {
-		html = readFileToString("static/index.htm");
-	}
-	catch (const std::exception& e) {
-		std::cerr << "Error: " << e.what() << std::endl;
-		return 1;
-	}
+	//std::string html;
+	//try {
+	//	html = readFileToString("static/index.htm");
+	//}
+	//catch (const std::exception& e) {
+	//	std::cerr << "Error: " << e.what() << std::endl;
+	//	return 1;
+	//}
 
-	svr.Get("/api/data", [html](const httplib::Request&, httplib::Response& res) {
+	svr.Get("/", [](const httplib::Request&, httplib::Response& res) {
+		std::string html = readFileToString("static/index.htm");
 		res.set_content(html, "text/html");
 	});
+
+	svr.Post("/api/msg", [&cli](const httplib::Request& req, httplib::Response& res) {
+		auto history = json::parse(req.body)["history"];
+		std::string systemPrompt = readFileToString("systemPrompt.txt");
+		systemPrompt += getDateTime();
+
+		json systemMsg = {
+			{"role", "system"},
+			{"content", systemPrompt}
+		};
+
+		history.insert(history.begin(), systemMsg);
+
+		json request = {
+			{"model", "local-model"},
+			{"messages", history},
+			{"temperature", 0.7}
+		};
+
+		auto gpt = cli.Post("/v1/chat/completions", request.dump(), "application/json");
+		json response;
+
+		if (!gpt || gpt->status != 200) {
+			response["type"] = "error";
+			response["content"] = "";
+		}
+		else {
+			std::string ans = json::parse(gpt->body)["choices"][0]["message"]["content"];
+			response["type"] = "message";
+			response["content"] = ans;
+		}
+
+		res.set_content(response.dump(), "application/json");
+	});
+
+	std::cout << "[ OK ]\n";
 	svr.listen("0.0.0.0", 8080);
+	return 0;
 }
